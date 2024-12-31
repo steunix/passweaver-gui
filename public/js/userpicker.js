5a9e8ed3f5476fde387c51be6880de8d9c61153d
Stefano Rivoir <rs4000@gmail.com>
2024-12-31 10:36:24 +0100

    Add scope selection in one time secrets (#163)
    Fixes #162

--- public/js/userpicker.js
+++ public/js/userpicker.js
@@ -29,8 +29,8 @@ async function search () {
   if (body.data.length) {
     let row = ''
     for (const usr of body.data) {
-      row += `<tr id='row-${usr.id}' data-id='${usr.id}' style='cursor:pointer;'>`
-      row += `<td><sl-icon-button id='user-${usr.id}' data-id='${usr.id}' name='arrow-right-circle'></sl-icon-button></td>`
+      row += `<tr id='row-${usr.id}' data-id='${usr.id}' data-desc='${usr.lastname} ${usr.firstname}' style='cursor:pointer;'>`
+      row += `<td><sl-icon-button id='user-${usr.id}' data-id='${usr.id}' data-desc='${usr.lastname} ${usr.firstname}' name='arrow-right-circle'></sl-icon-button></td>`
       row += `<td>${usr.login}</td>`
       row += `<td>${usr.lastname} ${usr.firstname}</td>`
       row += '</tr>'
@@ -40,10 +40,10 @@ async function search () {
 
   // Event handlers
   JH.event('#userpickertable tbody tr[id^=row]', 'dblclick', (ev) => {
-    userCallback(ev.currentTarget.getAttribute('data-id'))
+    userCallback(ev.currentTarget.getAttribute('data-id'), ev.currentTarget.getAttribute('data-desc'))
   })
   JH.event('#userpickertable tbody sl-icon-button[id^=user]', 'click', (ev) => {
-    userCallback(ev.currentTarget.getAttribute('data-id'))
+    userCallback(ev.currentTarget.getAttribute('data-id'), ev.currentTarget.getAttribute('data-desc'))
   })
 }
 
