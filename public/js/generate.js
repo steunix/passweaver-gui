4012b1720773c47c7f8bb2c9732c68067fa97446
steunix <stefano.rivoir@tesisquare.com>
2025-02-09 21:00:42 +0100

    Refactor password generation

--- public/js/generate.js
+++ public/js/generate.js
@@ -12,29 +12,19 @@ const domCache = {
   noSymGeneratedPasswordInput: JH.query('#generatedpasswordns')
 }
 
-async function passwordGenerate () {
-  JH.value(domCache.passwordInput, 'Generating...')
-  const resp = await JH.http('/api/generatepassword')
+async function generatePassword (symbols = true) {
+  const passwordInput = symbols ? domCache.passwordInput : domCache.noSymPasswordInput
+  JH.value(passwordInput, 'Generating...')
+  const url = symbols ? '/api/generatepassword' : '/api/generatepassword?symbols=false'
+
+  const resp = await JH.http(url)
   if (!await PW.checkResponse(resp)) {
     return
   }
 
   const body = await resp.json()
   if (body.status === 'success') {
-    JH.value(domCache.passwordInput, body.data.password)
-  }
-}
-
-async function passwordGenerateNoSymbols () {
-  JH.value(domCache.noSymPasswordInput, 'Generating...')
-  const resp = await JH.http('/api/generatepassword?symbols=false')
-  if (!await PW.checkResponse(resp)) {
-    return
-  }
-
-  const body = await resp.json()
-  if (body.status === 'success') {
-    JH.value(domCache.noSymPasswordInput, body.data.password)
+    JH.value(passwordInput, body.data.password)
   }
 }
 
@@ -42,21 +32,23 @@ function tokenGenerate (data) {
   window.location = `/pages/onetimesecret?data=${encodeURIComponent(data)}`
 }
 
-await passwordGenerate()
-await passwordGenerateNoSymbols()
+// Initial password generation
+await generatePassword()
+await generatePassword(false)
 
-JH.event(domCache.generateButton, 'click', async (ev) => {
-  await passwordGenerate()
+// Event listeners
+JH.event(domCache.generateButton, 'click', async () => {
+  await generatePassword()
 })
 
-JH.event(domCache.noSymGenerateButton, 'click', async (ev) => {
-  await passwordGenerateNoSymbols()
+JH.event(domCache.noSymGenerateButton, 'click', async () => {
+  await generatePassword(false)
 })
 
-JH.event(domCache.onetimeCreate, 'click', async (ev) => {
+JH.event(domCache.onetimeCreate, 'click', () => {
   tokenGenerate(JH.value(domCache.generatedPasswordInput))
 })
 
-JH.event(domCache.noSymOnetimeCreate, 'click', async (ev) => {
+JH.event(domCache.noSymOnetimeCreate, 'click', () => {
   tokenGenerate(JH.value(domCache.noSymGeneratedPasswordInput))
 })
