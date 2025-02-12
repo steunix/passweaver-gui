ad9d6f275398d3b5137755959e101777bcc0b833
steunix <stefano.rivoir@tesisquare.com>
2025-02-12 20:14:54 +0100

    Auto run Passweaver GUI for tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
-// @ts-check
-import { defineConfig, devices } from '@playwright/test';
+// @ts-chec
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -30,7 +30,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
@@ -42,13 +42,13 @@ export default defineConfig({
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      use: { ...devices['Desktop Safari'] }
+    }
 
     /* Test against mobile viewports. */
     // {
@@ -72,10 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
-
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3151',
+    reuseExistingServer: !process.env.CI
+  }
+})
