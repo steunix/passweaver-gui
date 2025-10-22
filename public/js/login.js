e59c6ba7731ed96fce7a4ac1c90cae54f8562910
Stefano Rivoir <stefano.rivoir@tesisquare.com>
2025-10-22 08:16:20 +0200

    When loggin in, disable all login buttons Fixes #510

--- public/js/login.js
+++ public/js/login.js
@@ -2,10 +2,13 @@ import * as JH from './jh.js'
 
 JH.event('#loginForm', 'submit', () => {
   JH.disable('#login')
+  JH.disable('#glogin')
   JH.query('#login').innerHTML = 'Loggin in...'
 })
 
 JH.event('#glogin', 'click', async () => {
+  JH.disable('#login')
+  JH.disable('#glogin')
   const viewitem = JH.value('#viewitem')
   const resp = await JH.http(`/login/google/url?${viewitem ? `viewitem=${encodeURIComponent(viewitem)}` : ''}`)
   window.location.href = (await resp.json()).url
