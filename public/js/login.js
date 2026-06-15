62e860b56bd1f145753bd7f6c300d885ecc6b045
Stefano Rivoir <stefano.rivoir@tesisquare.com>
2026-06-15 14:59:11 +0200

    Add "logging in" dialog in login page Fixes #718

--- public/js/login.js
+++ public/js/login.js
@@ -3,12 +3,13 @@ import * as JH from './jh.js'
 JH.event('#loginForm', 'submit', () => {
   JH.disable('#login')
   JH.disable('#glogin')
-  JH.query('#login').innerHTML = 'Loggin in...'
+  JH.query('#loggingdialog').open = true
 })
 
 JH.event('#glogin', 'click', async () => {
   JH.disable('#login')
   JH.disable('#glogin')
+  JH.query('#loggingdialog').open = true
   const viewitem = JH.value('#viewitem')
   const viewfolder = JH.value('#viewfolder')
   const resp = await JH.http(`/login/google/url?${viewitem ? `viewitem=${encodeURIComponent(viewitem)}` : ''}${viewfolder ? `&viewfolder=${encodeURIComponent(viewfolder)}` : ''}`)
